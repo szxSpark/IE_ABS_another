73628700e9f1966d74d547e202b6dab8f5413f40
szxSpark <szx_spark@outlook.com>
2020-05-07 23:53:09 +0800

    .

--- preprocess/scripts.py
+++ preprocess/scripts.py
@@ -2,7 +2,9 @@ import json
 from utils.Constants import E1_R_WORD, R_E2_WORD
 def func1():
     # 构建subword的svo
-    hier_json = "/home/zxsong/workspace/seass/data/toutiao_word/train/train.hierarchical.combine.json"
+    # hier_json = "/home/zxsong/workspace/seass/data/toutiao_word/train/train.hierarchical.combine.json"
+    hier_json = "/home/zxsong/workspace/seass/data/toutiao_word/dev/valid.hierarchical.combine.json"
+
     id2svo = {}
     for data in json.load(open(hier_json, "r", encoding="utf-8")):
         article, abstract = data['article'], data['abstract']
@@ -21,9 +23,11 @@ def func1():
             svo_words.append(e1 + r + e2)
         origin_id = data['origin_id']
         id2svo[origin_id] = svo_words
-
-    data_f = "/home/zxsong/workspace/seass/data/toutiao_word/train/train.title.txt"
-    out_f = "/home/zxsong/workspace/seass/data/toutiao_word/train/subword/svo.tmp.txt"
+    print(len(id2svo))
+    # data_f = "/home/zxsong/workspace/seass/data/toutiao_word/train/train.title.txt"
+    # out_f = "/home/zxsong/workspace/seass/data/toutiao_word/train/subword/svo.tmp.txt"
+    data_f = "/home/zxsong/workspace/seass/data/toutiao_word/dev/valid.title.txt"
+    out_f = "/home/zxsong/workspace/seass/data/toutiao_word/dev/subword/svo.tmp.txt"
     c = 0
     with open(out_f, "w", encoding="utf-8") as f:
         for i, title in enumerate(open(data_f, "r", encoding="utf-8")):
@@ -37,19 +41,23 @@ def func1():
 
 def func2():
     # 逆操作
-    hier_json = "/home/zxsong/workspace/seass/data/toutiao_word/train/train.hierarchical.combine.json"
+    # hier_json = "/home/zxsong/workspace/seass/data/toutiao_word/train/train.hierarchical.combine.json"
+    hier_json = "/home/zxsong/workspace/seass/data/toutiao_word/dev/valid.hierarchical.combine.json"
     id2svo_num = {}
     for data in json.load(open(hier_json, "r", encoding="utf-8")):
         svo = data["svo"]
         origin_id = data['origin_id']
         id2svo_num[origin_id] = len(svo)
 
-    data_f = "/home/zxsong/workspace/seass/data/toutiao_word/train/train.title.txt"
+    # data_f = "/home/zxsong/workspace/seass/data/toutiao_word/train/train.title.txt"
+    data_f = "/home/zxsong/workspace/seass/data/toutiao_word/dev/valid.title.txt"
     data_num = 0
     for i, title in enumerate(open(data_f, "r", encoding="utf-8")):
         data_num += 1
 
-    in_f = "/home/zxsong/workspace/seass/data/toutiao_word/train/subword/svo.tmp.subword.txt"
+    # in_f = "/home/zxsong/workspace/seass/data/toutiao_word/train/subword/svo.tmp.subword.txt"
+    in_f = "/home/zxsong/workspace/seass/data/toutiao_word/dev/subword/svo.tmp.subword.txt"
+
     c = 0
     one_svo = ""
     final_svo = []
@@ -68,7 +76,8 @@ def func2():
             one_svo += " "+R_E2_WORD+" "
     assert c % 3 == 0
     i = 0
-    with open("/home/zxsong/workspace/seass/data/toutiao_word/train/subword/svo.subword.txt",
+    # with open("/home/zxsong/workspace/seass/data/toutiao_word/train/subword/svo.subword.txt",
+    with open("/home/zxsong/workspace/seass/data/toutiao_word/dev/subword/svo.subword.txt",
               "w", encoding="utf-8")as f:
         for data_i in range(data_num):
             svo_num = id2svo_num[data_i] if data_i in id2svo_num else 0
@@ -81,4 +90,5 @@ def func2():
 
 
 if __name__ == "__main__":
+    func1()
     func2()
