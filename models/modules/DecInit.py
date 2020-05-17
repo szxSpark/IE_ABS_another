9a7a90be13c308a864031a162d8c57860649de77
szxSpark <szx_spark@outlook.com>
2020-05-17 21:57:09 +0800

    .

--- models/modules/DecInit.py
+++ models/modules/DecInit.py
@@ -3,9 +3,11 @@ import torch.nn as nn
 class DecInit(nn.Module):
     def __init__(self, opt):
         super(DecInit, self).__init__()
-        self.num_directions = 2 if opt.brnn else 1
-        assert opt.enc_rnn_size % self.num_directions == 0
-        self.enc_rnn_size = opt.enc_rnn_size
+        # self.num_directions = 2 if opt.brnn else 1
+        # assert opt.enc_rnn_size % self.num_directions == 0
+        # self.enc_rnn_size = opt.enc_rnn_size
+        self.enc_rnn_size = 512
+
         self.dec_rnn_size = opt.dec_rnn_size
         self.initer = nn.Linear(self.enc_rnn_size, self.dec_rnn_size)
 
