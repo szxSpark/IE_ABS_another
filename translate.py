4692fc9995fba2b6e113a8d211e55de4a851402c
zxSong <szx_spark@outlook.com>
2020-07-02 22:14:37 +0800

    .

--- translate.py
+++ translate.py
@@ -1,29 +1,12 @@
 from __future__ import division
-import argparse
 import torch
 import torch.nn as nn
-from models import Optim, NMTModel, Encoder, Decoder, DecInit, Generator
-from torch import cuda
-import math
-import time
-import logging
-import numpy as np
-import random
-from PyRouge.Rouge import Rouge
-import xargs
-from utils import onlinePreprocess
-from utils.onlinePreprocess import prepare_data_online
-import utils.Constants as Constants
+from models import NMTModel, Encoder, Decoder, DecInit, Generator
 from utils.Translator import Translator
-from utils.Dataset import Dataset
 import os
 import re
 from utils.Constants import E1_R_WORD, R_E2_WORD
-import sys
-from tqdm import tqdm
-import json
 from oie_extraction import extract_elements
-
 from pyltp import Segmentor
 LTP_DIR = "/home/user-2/zxsong_github/MonitorSystem/lib/ltp_data_v3.4.0"
 segmentor = Segmentor()
@@ -143,36 +126,6 @@ def magic_data_process(translator, article):
     data = translator.buildData(src_batch, None, spo_batch)
     return data
 
-def compute_loss(g_outputs, g_targets, generator, crit, g_p_gens, g_attn, enc_batch_extend_vocab, extra_zeros, coverage_losses):
-    # g_outputs: (decL-1, B, H)
-    # g_targets: (decL-1, B)
-    # g_p_gens:  (decL-1, B, 1)
-    # coverage_losses  (decL-1, B)
-    if opt.pointer_gen:
-        dec_padding_mask = 1 - g_targets.eq(Constants.PAD)  # (decL-1, B)
-        dec_padding_mask = dec_padding_mask.view(-1).float()  # (decL-1*B,)
-        g_prob_t = generator(g_outputs, g_p_gens, g_attn, enc_batch_extend_vocab, extra_zeros)  # (decL-1*B, tgt_size)
-        target = g_targets.view(-1)  # (decL-1*B,)
-        gold_probs = torch.gather(g_prob_t, 1, target.unsqueeze(1)).squeeze()  # (decL-1*B,)  目的是拿到金的
-        step_loss = -torch.log(gold_probs + 1e-12)  # (decL-1*B,)  generator应该是sm
-        nll_loss = torch.sum(step_loss * dec_padding_mask).item()
-        if opt.is_coverage:
-            step_loss = step_loss + coverage_losses.view(-1)  # (decL-1*B,)
-        step_loss = step_loss * dec_padding_mask  # (decL-1*B,)
-        total_loss = torch.sum(step_loss)
-        report_loss = total_loss.item()
-        if opt.is_coverage:
-            coverage_report_loss = torch.sum(coverage_losses.view(-1)*dec_padding_mask).item()
-            return total_loss, (report_loss, nll_loss, coverage_report_loss), 0
-        return total_loss, report_loss, 0
-    else:
-        g_out_t = g_outputs.view(-1, g_outputs.size(2))
-        g_prob_t = generator(g_out_t)
-        g_loss = crit(g_prob_t, g_targets.view(-1))
-        total_loss = g_loss
-        report_loss = total_loss.item()
-        return total_loss, report_loss, 0
-
 def load_model(model_file):
     checkpoint = torch.load(model_file, map_location=torch.device('cpu'))
     vocab_dicts = checkpoint['dicts']
@@ -227,24 +180,17 @@ def predict_oneData(translator, data):
     result = fouput.readlines()
     return "".join(result[0].split(" "))
 
-def main():
-    model_file = "./checkpoints/model_devRouge_0.6756_0.2952_e62.pt"
+model_file = "./checkpoints/model_devRouge_0.6756_0.2952_e62.pt"
+def summarize(article):
     model, opt, vocab_dicts = load_model(model_file)
     translator = Translator(opt, model, vocab_dicts)
 
-    # article = '温州网讯昨天，广受关注的“瑞安孕妇重度烧伤”一事有了新进展：家属称，王芙蓉现阶段治疗急需A型血小板；事故初步认定为由燃气泄漏引起的爆炸事故。5月9日晚11时许，怀孕8个月的王芙蓉和母亲在厨房煮夜宵时，厨房里突然发生爆燃，一家四口不同程度烧伤。据悉，目前捐给王芙蓉一家的爱心款累计突破600万元。王芙蓉已有较清晰的意识王芙蓉的叔叔王先生说，5月20日，王芙蓉做了清理坏死皮肤的手\n术，术后恢复情况较为理想，虽还不能说话，但已有较清晰的意识。王芙蓉的丈夫和她的父母也恢复得不错。但是让家属担心的是，前天医院\n通知说王芙蓉需要A型血小板，但目前瑞安市血库存量不多，这几天他们正发动亲戚朋友捐A型血小板。瑞安市血站得知这一情况后，第一时间\n通知志愿者前来献血。据血站的相关人员介绍，目前已有4名志愿者提供血小板，可满足本周用量，但如果下周一还是需要血小板的话，就需要社会上的好心人继续献出爱心。据介绍，想为王芙蓉捐A型血小板的热心市民，可到瑞安市血站献血，也可直接到市中心血站献血，献血时向工作人员表明是献给王芙蓉即可。调查组初步认定4点结果事发后，瑞安立刻成立事故调查组展开调查。据事故调查组组长、瑞安市安监局副局长顾荣华介绍，目前该起事故已有初步认定结果：一是此次事故的性质已基本确认为燃气泄漏引起的爆炸事故；二是爆炸部位也已基本确认，是\n位于厨房间洗碗水槽下方的一个密闭的空间；三是爆炸主因，燃气泄漏扩散蔓延，与空气混合成爆炸性气体，当浓度达到爆炸极限下限时，遇\n到点火源，瞬间产生爆炸；四是排除由户外管道燃气泄漏引起爆炸的可能性。据调查组介绍，下一步将根据相关规定查清事故责任，主要是要\n查明这起事故是不是生产安全责任事故。本文转自：温州网'  # str
-    # article = '四海网讯，\n近日，有媒体报道称：章子怡真怀孕了!报道还援引知情人士消息称，“章子怡怀孕大概四五个月，预产期是年底前后，现在已经不接工作了。”这到底是怎么回事?消息是真是假?针对此消息，23日晚8时30分，华西都市报记者迅速联系上了与章子怡家里关系极好的知情人士，这位人士向华西都市报记者证实说：“子怡这次确实怀孕了。她已经36岁了，也该怀孕了。章子怡怀上汪峰的孩子后，子怡的父母亲十分高兴。子怡的母亲，已开始悉心照料女儿了。子怡的预产期大概是今年12月底。”当晚9时，华西都市报记者为了求证章子怡怀孕消息，又电话联系章子怡的亲哥\n哥章子男，但电话通了，一直没有人<Paragraph>接听。有关章子怡怀孕的新闻自从2013年9月份章子怡和汪峰恋情以来，就被传N遍了!不过，\n时间跨入2015年，事情却发生着微妙的变化。2015年3月21日，章子怡担任制片人的电影《从天儿降》开机，在开机发布会上几张合影，让网友又燃起了好奇心：“章子怡真的怀孕了吗?”但后据证实，章子怡的“大肚照”只是影片宣传的噱头。过了四个月的7月22日，《太平轮》新一轮宣\n传，章子怡又被发现状态不佳，不时深呼吸，不自觉想捂住肚子，又觉得不妥。然后在8月的一天，章子怡和朋友吃饭，在酒店门口被风行工作室拍到了，疑似有孕在身!今年7月11日，汪峰本来在上海要举行演唱会，后来因为台风“灿鸿”取消了。而消息人士称，汪峰原来打算在演唱会\n上当着章子怡的面宣布重大消息，而且章子怡已经赴上海准备参加演唱会了，怎知遇到台风，只好延期，相信9月26日的演唱会应该还会有惊喜大白天下吧。'
-    article = '''
-    昨天（1日）有暴徒在港岛区非法集结，纵火、堵路等违法暴力行为重现街头，更有暴徒袭击警务人员，截至昨晚10时，警方共拘捕约370人。据悉其中包括3名海关人员。对此，香港特区政府海关关长邓以海表示震怒，强调纪律部队人员必须守法，绝不姑息违纪违法人员。
-昨日，有暴徒在铜锣湾一带堵路纵火。
-
-  昨天中午起，有暴徒在港岛区一带进行暴力破坏行动。他们掘起地砖、纵火堵路、肆意破坏店铺等，严重危害公共秩序和公共安全。截至昨晚10时，警方拘捕约370人，被捕人涉嫌非法集结、公众地方行为不检、疯狂驾驶和管有攻击性武器等。据了解，3名海关人员也在示威中被捕。
-
-  邓以海表示，对3名同事违法感到震怒，重申纪律部队人员必须守法，支持特区政府和执法单位止暴制乱，承诺会对违纪违法人员严肃处理，绝不姑息。
-    '''
     data = magic_data_process(translator, article)  # 已经分好batch Dataset， list((src_batch, tgt_batch)))
     summary = predict_oneData(translator, data)
-    print(summary)
+    return summary
 
 if __name__ == "__main__":
-    main()#
+    # article = '温州网讯昨天，广受关注的“瑞安孕妇重度烧伤”一事有了新进展：家属称，王芙蓉现阶段治疗急需A型血小板；事故初步认定为由燃气泄漏引起的爆炸事故。5月9日晚11时许，怀孕8个月的王芙蓉和母亲在厨房煮夜宵时，厨房里突然发生爆燃，一家四口不同程度烧伤。据悉，目前捐给王芙蓉一家的爱心款累计突破600万元。王芙蓉已有较清晰的意识王芙蓉的叔叔王先生说，5月20日，王芙蓉做了清理坏死皮肤的手\n术，术后恢复情况较为理想，虽还不能说话，但已有较清晰的意识。王芙蓉的丈夫和她的父母也恢复得不错。但是让家属担心的是，前天医院\n通知说王芙蓉需要A型血小板，但目前瑞安市血库存量不多，这几天他们正发动亲戚朋友捐A型血小板。瑞安市血站得知这一情况后，第一时间\n通知志愿者前来献血。据血站的相关人员介绍，目前已有4名志愿者提供血小板，可满足本周用量，但如果下周一还是需要血小板的话，就需要社会上的好心人继续献出爱心。据介绍，想为王芙蓉捐A型血小板的热心市民，可到瑞安市血站献血，也可直接到市中心血站献血，献血时向工作人员表明是献给王芙蓉即可。调查组初步认定4点结果事发后，瑞安立刻成立事故调查组展开调查。据事故调查组组长、瑞安市安监局副局长顾荣华介绍，目前该起事故已有初步认定结果：一是此次事故的性质已基本确认为燃气泄漏引起的爆炸事故；二是爆炸部位也已基本确认，是\n位于厨房间洗碗水槽下方的一个密闭的空间；三是爆炸主因，燃气泄漏扩散蔓延，与空气混合成爆炸性气体，当浓度达到爆炸极限下限时，遇\n到点火源，瞬间产生爆炸；四是排除由户外管道燃气泄漏引起爆炸的可能性。据调查组介绍，下一步将根据相关规定查清事故责任，主要是要\n查明这起事故是不是生产安全责任事故。本文转自：温州网'  # str
+    # article = '四海网讯，\n近日，有媒体报道称：章子怡真怀孕了!报道还援引知情人士消息称，“章子怡怀孕大概四五个月，预产期是年底前后，现在已经不接工作了。”这到底是怎么回事?消息是真是假?针对此消息，23日晚8时30分，华西都市报记者迅速联系上了与章子怡家里关系极好的知情人士，这位人士向华西都市报记者证实说：“子怡这次确实怀孕了。她已经36岁了，也该怀孕了。章子怡怀上汪峰的孩子后，子怡的父母亲十分高兴。子怡的母亲，已开始悉心照料女儿了。子怡的预产期大概是今年12月底。”当晚9时，华西都市报记者为了求证章子怡怀孕消息，又电话联系章子怡的亲哥\n哥章子男，但电话通了，一直没有人<Paragraph>接听。有关章子怡怀孕的新闻自从2013年9月份章子怡和汪峰恋情以来，就被传N遍了!不过，\n时间跨入2015年，事情却发生着微妙的变化。2015年3月21日，章子怡担任制片人的电影《从天儿降》开机，在开机发布会上几张合影，让网友又燃起了好奇心：“章子怡真的怀孕了吗?”但后据证实，章子怡的“大肚照”只是影片宣传的噱头。过了四个月的7月22日，《太平轮》新一轮宣\n传，章子怡又被发现状态不佳，不时深呼吸，不自觉想捂住肚子，又觉得不妥。然后在8月的一天，章子怡和朋友吃饭，在酒店门口被风行工作室拍到了，疑似有孕在身!今年7月11日，汪峰本来在上海要举行演唱会，后来因为台风“灿鸿”取消了。而消息人士称，汪峰原来打算在演唱会\n上当着章子怡的面宣布重大消息，而且章子怡已经赴上海准备参加演唱会了，怎知遇到台风，只好延期，相信9月26日的演唱会应该还会有惊喜大白天下吧。'
+    article = '''昨天（1日）有暴徒在港岛区非法集结，纵火、堵路等违法暴力行为重现街头，更有暴徒袭击警务人员，截至昨晚10时，警方共拘捕约370人。据悉其中包括3名海关人员。对此，香港特区政府海关关长邓以海表示震怒，强调纪律部队人员必须守法，绝不姑息违纪违法人员。昨日，有暴徒在铜锣湾一带堵路纵火。昨天中午起，有暴徒在港岛区一带进行暴力破坏行动。他们掘起地砖、纵火堵路、肆意破坏店铺等，严重危害公共秩序和公共安全。截至昨晚10时，警方拘捕约370人，被捕人涉嫌非法集结、公众地方行为不检、疯狂驾驶和管有攻击性武器等。据了解，3名海关人员也在示威中被捕。邓以海表示，对3名同事违法感到震怒，重申纪律部队人员必须守法，支持特区政府和执法单位止暴制乱，承诺会对违纪违法人员严肃处理，绝不姑息。'''
+    summary = summarize(article)
